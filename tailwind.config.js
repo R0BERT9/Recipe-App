0de480fd62287e6e9d46dc71d8c95f32814b4566
R0BERT9 <robertocarlos.perez141@gmail.com>
2024-07-09 09:24:39 -0500

    Vistas Creadas

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./www/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'fcp1': '#FFFFFF',
+        'fcp2': '#FFC107',
+        'fcp3': '#4CAF50',
+        'fcs1': '#F8F8F8',
+        'fcs2': '#BAB9B9',
+        'fcs3': '#EBEBEB',
+        'fcs4': '#828282'
+
+        
+      },
+    },
   },
   plugins: [],
 }
